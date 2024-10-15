f0d9c6dc5b02492bda9c37eec54d3c485f2f5978
Carlos <carlos.perea.figuera.al@iespoligonosur.org>
2024-10-15 22:27:13 +0200

    modelos creados

--- proyectoModelos/tutorial/models.py
+++ proyectoModelos/tutorial/models.py
@@ -1,21 +1,80 @@
 from django.db import models
 
-# Create your models here.
-
 class Usuario(models.Model):
-    nombre = models.CharField()
-    email = models.EmailField()
-    fecha_Registro = models.DateField()
-    es_activo = models.BooleanField()
-    puntuacion = models.DecimalField()
-    
-class Perfil (models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    fecha_Registro = models.DateField(auto_now=True)
+    es_activo = models.BooleanField(default=False)
+    puntuacion = models.DecimalField(max_digits=3, decimal_places=1)  
+
+class Perfil(models.Model):
     bio = models.TextField()
-    fecha_Nacimiento = models.DateField()
+    fecha_Nacimiento = models.DateField()  
     REDES = [
-        ("Instagram"),
-        ("Facebook"),
-        ("Twitter"),
-        ("LinkedIn"),
+        ("IG", "Instagram"),
+        ("FB", "Facebook"),
+        ("TW", "Twitter"),
+        ("LI", "LinkedIn"),
     ]
-    redes = models.CharField(choices=REDES)
+    redes = models.CharField(choices=REDES, max_length=50)
+    estudios = models.TextField(max_length=100)
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="perfiles_Usuarios")
+
+class Tutorial(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.TextField()  
+    fecha_Creacion = models.DateTimeField(auto_now=True)
+    visitas = models.IntegerField()
+    valoracion = models.DecimalField(max_digits=3, decimal_places=1)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="usuario_del_tutorial")
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    es_activa = models.BooleanField(default=False)
+    popularidad = models.DecimalField(max_digits=3, decimal_places=1)
+    descripcion = models.TextField(max_length=300)
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE, related_name="categoria_del_tutorial")
+
+class SubCategoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=300)
+    fecha_Creacion = models.DateTimeField(auto_now=True)
+    activa = models.BooleanField(default=False)
+    categoria = models.OneToOneField(Categoria, on_delete=models.CASCADE, related_name="CategoriaSubcategoria")
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    publica = models.BooleanField(default=False)
+    descripcion = models.TextField(max_length=300)
+    tutorial = models.ManyToManyField(Tutorial, related_name="Etiquetas_del_tutorial")
+
+class Favorito(models.Model):
+    fecha_Guardado = models.DateTimeField(auto_now=True)
+    comentario_Destacado = models.TextField(max_length=300)
+    importancia = models.DecimalField(max_digits=3, decimal_places=1)
+    notificacion = models.BooleanField(default=False)
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE, related_name="Tutoriales_favoritos")
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="Favoritos_del_usuario")  
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=300)
+    horas = models.IntegerField()
+    precio = models.DecimalField(max_digits=5, decimal_places=2)  
+    usuario = models.ManyToManyField(Usuario, related_name="usuarios_del_curso")
+
+class Certificado(models.Model):
+    fecha_emision = models.DateField(auto_now=True)
+    codigo_verificacion = models.CharField(max_length=50)
+    nivel = models.IntegerField()
+    url_descarga = models.CharField(max_length=50)
+    curso = models.OneToOneField(Curso, on_delete=models.CASCADE, related_name="certificado_Curso") 
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="Certificado_Usuarios")
+
+class Comentario(models.Model):
+    contenido = models.TextField(max_length=300)
+    fecha = models.DateTimeField()
+    visible = models.BooleanField(default=False)  # Corregido vivible a visible
+    puntuacion = models.DecimalField(max_digits=3, decimal_places=1)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="Comentarios_de_Usuarios")  

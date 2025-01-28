5afe3235730b79e490c6e9e98ce86246fba41b1c
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2025-01-28 12:00:42 +0100

    terminar hoy

--- proyectoModelos/tutorial/serializers.py
+++ proyectoModelos/tutorial/serializers.py
@@ -17,3 +17,14 @@ class TutorialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tutorial
         fields = ('titulo', 'contenido', 'fecha_Creacion', 'visitas', 'valoracion', 'usuario')
+        
+#clase cursos serializers
+
+class CursosSerializer(serializers.ModelSerializer):
+    #Paras las relaciones One y Many to One
+    usuario = UsuarioSerializer()
+    tutorial = TutorialSerializer()
+    
+    class Meta:
+        model = Curso
+        fields = ('nombre', 'descripcion', 'horas', 'precio', 'usuario', 'tutorial')
